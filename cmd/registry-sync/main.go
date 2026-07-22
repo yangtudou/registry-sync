d6f0bbd50dfd2de87b103d1f89198f8a7986b093
yyysay-work-0 <otherappleidtest@outlook.com>
2026-07-22 11:02:12 +0800

    feat(cli): add validate command

--- cmd/registry-sync/main.go
+++ cmd/registry-sync/main.go
@@ -12,6 +12,7 @@ import (
 	"github.com/yyysay/registry-sync/internal/mapper"
 	"github.com/yyysay/registry-sync/internal/output"
 	"github.com/yyysay/registry-sync/internal/task"
+	"github.com/yyysay/registry-sync/internal/validate"
 )
 
 func main() {
@@ -23,6 +24,8 @@ func main() {
 	switch os.Args[1] {
 	case "plan":
 		plan(os.Args[2:])
+	case "validate":
+		validateCommand(os.Args[2:])
 	default:
 		log.Fatalf("unknown command: %s", os.Args[1])
 	}
@@ -30,10 +33,11 @@ func main() {
 
 func usage() {
 	fmt.Println("usage:")
-	fmt.Println("  registry-sync plan [options]")
+	fmt.Println("  registry-sync <command>")
 	fmt.Println()
 	fmt.Println("commands:")
 	fmt.Println("  plan")
+	fmt.Println("  validate")
 }
 
 func plan(args []string) {
@@ -84,4 +88,43 @@ func plan(args []string) {
 	default:
 		output.PrintText(tasks)
 	}
+}
+
+func validateCommand(args []string) {
+	fs := flag.NewFlagSet("validate", flag.ExitOnError)
+
+	configFile := fs.String(
+		"config",
+		"config.yaml",
+		"config file",
+	)
+
+	imageFile := fs.String(
+		"images",
+		"images.txt",
+		"image list file",
+	)
+
+	fs.Parse(args)
+
+	cfg, err := config.Load(*configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := validate.Config(cfg); err != nil {
+		log.Fatal(err)
+	}
+
+	images, err := image.Load(*imageFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := validate.Images(images); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("config ok")
+	fmt.Println("images ok")
 }
