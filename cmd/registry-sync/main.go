6dc04140e7c43356eb4de01e28189e15c83d3c5d
yyysay-work-0 <otherappleidtest@outlook.com>
2026-07-22 10:50:56 +0800

    feat(cli): add plan command

--- cmd/registry-sync/main.go
+++ cmd/registry-sync/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/yyysay/registry-sync/internal/config"
 	"github.com/yyysay/registry-sync/internal/destination"
@@ -12,6 +13,22 @@ import (
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("usage: registry-sync <command>")
+		fmt.Println("commands:")
+		fmt.Println("  plan")
+		return
+	}
+
+	switch os.Args[1] {
+	case "plan":
+		plan()
+	default:
+		log.Fatalf("unknown command: %s", os.Args[1])
+	}
+}
+
+func plan() {
 	cfg, err := config.Load("config.yaml")
 	if err != nil {
 		log.Fatal(err)
